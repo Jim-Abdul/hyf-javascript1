2bbb267bbe3ed8d74d4587e13d222cafab0386ed
Jim-Abdul <33630161+Jim-Abdul@users.noreply.github.com>
2017-12-27 14:54:59 +0100

    Update Christmas.js

--- week1/Christmas.js
+++ week1/Christmas.js
@@ -1,29 +1,30 @@
 
 //1. Write a console.log statement saying "Hello World!" for each language that you know.
-//For example:
-//Halo, dunia! // Indonesian
-//Ciao, mondo! // Italian
-//Hola, mundo! // Spanish
+
 console.log("Hoi wereld!");
 console.log("Hi world!");
+
+
 //2. Consider the following code:
 //console.log('I'm awesome');
 //Copy the code in your.js file and run it.You will see that you will get a SyntaxError.Find a solution for this error.Hint: read the error message carefully, it also gives an indication of where the problem is.
+
 console.log("I'm awesome");
 
 
 //3. Declare a variable x and initialize it with an integer, using these exact steps:
 //3.1 First, declare your variable x(do not initialize it yet).
 //3.2 Add a console.log statement that explains that explains in words what you think the value of x is, like in this example:
-let x ="";
-console.log('the value of my variable x will be the quantity of passengers');       
 //3.3 Add a console.log statement that logs the value of x.
-console.log(x);
 //3.4 Now initialize your variable x with an integer.
-let x = 6;
 //3.5 Next, add a console.log statement that explains what you think the value of x is.
-console.log(3);
 //3.6 Add a console.log statement that logs the value of x.Steps to be taken:
+
+console.log('the value of my variable x will be the quantity of passengers');       
+console.log(x);
+let x = 6;
+console.log(3);
+
 
 
 //4. Declare a variable y and assign a string to it.
@@ -41,69 +42,76 @@ console.log('my favorite animal');
 
 //5. How do you round the number 7.25, to the nearest integer(i.e., whole number) ?
 //5.1 Declare a variable z and assign the number 7.25 to it.
-var z = 7.25;
 //5.2 Console.log z.
-console.log(z);
 //5.3 Declare another variable a that has the value of z but rounded to the nearest integer.
-var a = Math.round(z);
 //5.4 Console.log a.
-console.log(a);
 //5.5 So now we have z and a find a way to compare the two values and store the highest of the two in a new variable.
+
+var z = 7.25;
+console.log(z);
+var a = Math.round(z);
+console.log(a);
+
 var m ="";
 if (z > a) {
     var m = z;
  }
 else{var m = a}
-//5.6 Console.log the highest value.
+
 console.log(m);
 
 //6. Arrays!
 //6.1 Declare an empty array.Make sure that the name you choose indicates 'plurality', because an array is capable of containing more than one element. (See Naming conventions.)
-var myFavoriteAnimals = ["","",""];
 //6.2 Write a console.log statement that explains in words what you think the value of the array is.
-console.log("myFavoriteAnimals");
 //6.3 Console.log your array.
-console.log(myFavoriteAnimals);
 //6.4 Create an array that has your favorite animals inside(see if you can find a good name that exactly describes what this variable will hold).
-var myFavoriteAnimals = ["Cat","Dog","Tiger","Eagle"];
 //6.5 Log your array.
-console.log(myFavoriteAnimals);
 //6.6 Add a statement that adds Daan's favorite animal ('baby pig') to the existing array.
-myFavoriteAnimals.push('baby pig');
 //6.7 Log your new array!
+
+var myFavoriteAnimals = ["","",""];
+console.log("myFavoriteAnimals");
+console.log(myFavoriteAnimals);
+var myFavoriteAnimals = ["Cat","Dog","Tiger","Eagle"];
+console.log(myFavoriteAnimals);
+myFavoriteAnimals.push('baby pig');
 console.log(myFavoriteAnimals);
 
 //7. More strings
 //7.1 Let's consider the following string: let myString = "this is a test".
-let myString = "Hack my heart if you can"
 //7.2 Add the string to your file and console.log it.
 //7.3 Find a way to get the length of myString.
-let length = myString.length;
 //7.4 Console.log the length of myString.
+
+let myString = "Hack my heart if you can"
+let length = myString.length;
 console.log(myString.length);
 
 
 
 //8. Write a program that checks the types of two variables and prints out SAME TYPE if they are the same type.
 //8.1 First declare at least four variables and assign them different data types.
+//8.2 For each variable write a console.log statement that logs the value
+//8.3 Now write a console.log statement wherein you first explain in words what you think the type of your variables is.
+//8.4 Now use typeof to log the actual type of your variables.
+//8.5 Now compare the types of your different variables with one another.
+//8.6 Make sure to also show a message when the variables you are comparing are not the same type.
+
 let name = 'Jim';
 let age = 34;
 let arr = ['red', 'blue', 'black'];
 let answer = false;
-//8.2 For each variable write a console.log statement that logs the value
+
 console.log('The value of my variable name is: ' + name);
 console.log('The value of my variable age is: ' + age);
 console.log('The value of my variable arr is: ' + arr);
 console.log('The value of my variable answer is: ' + answer);
-//8.3 Now write a console.log statement wherein you first explain in words what you think the type of your variables is.
-console.log('the value of variable w will be my weight ');
-//8.4 Now use typeof to log the actual type of your variables.
+
 console.log(typeof name);
 console.log(typeof age);
 console.log(typeof arr);
 console.log(typeof answer);
-//8.5 Now compare the types of your different variables with one another.
-//8.6 Make sure to also show a message when the variables you are comparing are not the same type.
+
 if (name === age) {
     console.log(name + 'and' + age + 'are the same type');    
 } else if(name === arr){
@@ -122,19 +130,23 @@ if (name === age) {
 
 //9. If x equals 7, and the only other statement is x = x % 3, what would be the new value of x ?
 //9.1 Add at least 3 console.log statements in which you show that you understand what % does.
+
 'use strict';
 let x = 7;
 x = x % 3;
 console.log(x);
 //The value of x will be: 1
+
 let x = 7;
 x = x % 5;
 console.log(x);
 //The value of x will be : 2
+
 let x = 7;
 x = x % 4;
 console.log(x);
 //The value of x will be : 3
+
 console.log("the operation % calculate the Remainder of the division between the values ");
         
 //10. Write a program to answer the following questions:
